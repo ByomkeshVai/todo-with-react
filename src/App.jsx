c0f43f68dbe270bdd1d4999e2b2268151e24f626
byomkesh2580 <habibrahman2580@gmail.com>
2023-06-18 09:55:42 +0600

    todo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TaskBox from './components/TaskBox';
+
+const App = () => {
+  return (
+    <div className="bg-base-200 max-w-2xl mx-auto">
+      <TaskBox></TaskBox>
+    </div>
+  );
+};
+
+export default App;
